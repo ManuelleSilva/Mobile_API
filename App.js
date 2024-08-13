9a5e180f9b0cfa9d4f7cb7fd8f7c5eabb307ba27
ManuelleSilva <manuellemartins2007@gmail.com>
2024-08-12 22:11:47 -0300

    Atualização

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ScrollView } from 'react-native';
+
+const request = async (callback) => {
+  const response = await fetch('https://swapi.dev/api/starships/');
+  const parsed = await response.json();
+  callback(parsed.results);
+};
 
 export default function App() {
+  const [registros, setRegistros] = useState([]);
+
+  useEffect(() => {
+    request(setRegistros);
+  }, []);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Usando API do Star Wars</Text>
+      <FlatList
+        data={registros}
+        keyExtractor={(item) => item.name.toString()}
+        renderItem={({ item }) =>
+          <Text style={styles.items}>
+            <Text style={styles.item1}>Nave: {item.name} {'\n'}</Text>
+            <Text style={styles.item2}>Modelo: {item.model} {'\n'}</Text>
+            <Text style={styles.item3}>Usando:{item.manufacturer} {'\n'}</Text>
+          </Text>
+        }
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +38,29 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 22,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    marginTop:20,
+    fontSize: 30,
+    marginBottom:15,
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+  items:{
+    alignItems: 'left',
+    margin: 3,
+    backgroundColor: '#dfb911',
+    borderRadius: 5,
+    paddingLeft: 5,
+    paddingTop:5,
+  },
+  item1:{
+    fontSize:18,
+    fontWeight:'bold',
+  }
 });
